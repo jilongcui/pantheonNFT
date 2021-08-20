dea3b9b6823ee5223218c61a14ab38713ffb035f
jilongcui <jilongcui@163.com>
2021-08-20 12:32:57 +0800

    Use ERC721Card at migration

--- migrations/2_initial_migration.js
+++ migrations/2_initial_migration.js
@@ -1,6 +1,6 @@
 // migrations/2_deploy.js
 // SPDX-License-Identifier: MIT
-const ERC721PresetMinterPauserAutoId = artifacts.require("ERC721PresetMinterPauserAutoId");
+const ERC721Card = artifacts.require("ERC721Card");
 const PantheonC2C = artifacts.require("PantheonC2C");
 
 module.exports = function(deployer, network, accounts) {
@@ -10,7 +10,7 @@ module.exports = function(deployer, network, accounts) {
     tokenAddress = '0x63aA6b76120FBD69EA0ca0Ad61e6E342E2604e52';
   console.log(tokenAddress);
   // deployer.deploy(ERC721PresetMinterPauserAutoId, "PAN NFT","PANNFT", "https://my-json-server.typicode.com/jilongcui/pantheon_json_db/tokens/").then(function() {
-  deployer.deploy(ERC721PresetMinterPauserAutoId, "PAN NFT","PANNFT", "https://api.pantheon.best/tokens/").then(function() {
+  deployer.deploy(ERC721Card, "PAN NFT","PANNFT", "https://api.pantheon.best/tokens/").then(function() {
     return deployer.deploy(PantheonC2C, ERC721PresetMinterPauserAutoId.address,tokenAddress);
   });
 };
